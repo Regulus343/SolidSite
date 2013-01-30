b248462177be8db4a4a5f4d79b1fd2af923491ca
Regulus343 <codyjassman@gmail.com>
2013-01-29 21:27:34 -0700

    Created repository

---
+++ src/Regulus/SolidSite/SolidSite.php
@@ -0,0 +1,59 @@
+<?php namespace Regulus\SolidSite;
+
+/*----------------------------------------------------------------------------------------------------------
+	SolidSite
+		A composer package that assigns section name and titles to controller functions and simplifies
+		creation of breadcrumb trails and is useful for other components that require page identifying
+		information such as menus that highlight the current location.
+
+		created by Cody Jassman
+		last updated on January 29, 2013
+----------------------------------------------------------------------------------------------------------*/
+
+class SolidSite {
+
+	public static $trailItems = array();
+
+	/**
+	 * Add an item to the breadcrumb trail.
+	 *
+	 * @param  string   $title
+	 * @param  string   $uri
+	 * @return void
+	 */
+	public static function addTrailItem($title = '', $uri = '') {
+		if ($title != "") static::$trailItems[] = (object) array('title'=>$title, 'uri'=>$uri);
+	}
+
+	/**
+	 * Add an item to the breadcrumb trail.
+	 *
+	 * @return array
+	 */
+	public static function getTrailItems() {
+		return static::$trailItems;
+	}
+
+	/**
+	 * Create HTML for breadcrumb trail.
+	 *
+	 * @param  string   $title
+	 * @param  string   $uri
+	 * @return void
+	 */
+	public static function addTrailItem($title = '', $uri = '') {
+		$html = '';
+		if (!empty(static::$trailItems)) {
+			$html = '<ul>';
+			$first = true;
+			foreach (static::$trailItems[] as $trailItem) {
+				if (!$first) echo '&raquo;';
+				$html .= '<a href="'.URL::to($trailItem->uri).'">'.Format::entities($trailItem->title).'</a>';
+				$first = false;
+			}
+			$html .= '</ul>';
+		}
+		return $html;
+	}
+
+}
---
+++ src/Regulus/SolidSite/SolidSiteServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Regulus\SolidSite;
+
+use Illuminate\Support\ServiceProvider;
+
+class SolidSiteServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('regulus/solid-site');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
