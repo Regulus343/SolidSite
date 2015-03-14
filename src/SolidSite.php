689dc4cf58aabe039f3353820f115d8034ef4fb4
Cody Jassman <codyjassman@gmail.com>
2015-03-14 10:59:26 -0600

    changed last updated date

--- src/SolidSite.php
+++ src/SolidSite.php
@@ -8,7 +8,7 @@
 
 		created by Cody Jassman
 		v0.6.0
-		last updated on March 3, 2015
+		last updated on March 14, 2015
 ----------------------------------------------------------------------------------------------------------*/
 
 use Illuminate\Support\Facades\Config;
